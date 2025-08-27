d2507848315f7663377a0c0c2f6f18d7665fb2c0
danalmeidap <dbatistadealmeida23@gmail.com>
2025-08-27 18:50:21 -0300

    feat: Adjustments on level

--- level.py
+++ level.py
@@ -2,7 +2,6 @@ import const
 from pygame import Rect 
 
 
-
 class Level:
     def __init__(self, w, h):
         self.w = w
@@ -12,10 +11,10 @@ class Level:
     def draw(self, screen):
         for x in range(self.w):
             for y in range(self.h):
-                color = "gray" if self.tiles[x][y] == 1 else "black"
-                screen.draw.filled_rect(
-                    Rect((x * 32, y * 32), (32, 32)), color
-                )
+                rect = Rect(x * const.TILESIZE, y * const.TILESIZE,
+                            const.TILESIZE, const.TILESIZE)
+                color = const.GRAY if self.tiles[x][y] == 1 else const.BLACK
+                screen.draw.filled_rect(rect, color)
 
     def is_walkable(self, x, y):
         """Retorna True se o tile for andável"""
